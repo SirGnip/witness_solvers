e03f5553ced4a3ca05e1d3323e2eece08370eead
SirGnip <SirGnip@users.noreply.github.com>
2024-02-26 17:13:03 -0600

    Test deepcopy with Grid

--- src/test_puzzle.py
+++ src/test_puzzle.py
@@ -1,3 +1,4 @@
+import copy
 import puzzle
 
 def test_make_grid():
@@ -24,3 +25,16 @@ def test_make_grid_path():
 | | |
 O-+-+
     '''.strip()
+
+def test_grid_deepcopy():
+    grid = puzzle.Grid(3, 3)
+    a, b, c, d = (1, 1), (2, 1), (2, 2), (0, 1)
+    grid.edges[frozenset((a, b))] = True
+    grid.edges[frozenset((b, c))] = True
+
+    grid2 = copy.deepcopy(grid)
+    grid2.edges[frozenset((a, d))] = True
+
+    assert len([v for k, v in grid.edges.items() if v]) == 2
+    assert len([v for k, v in grid2.edges.items() if v]) == 3
+
