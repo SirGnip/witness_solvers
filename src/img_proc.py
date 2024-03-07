60e3e54a135e8c9dfbe8243f836f4c9538678cca
SirGnip <SirGnip@users.noreply.github.com>
2024-03-06 22:00:52 -0600

    Move functions from runner.py to img_proc.py

--- src/img_proc.py
+++ src/img_proc.py
@@ -1,6 +1,9 @@
 # Basic utils for interacting with an image
-from PIL import ImageGrab, ImageColor
+import ctypes
+from ctypes import wintypes
+from PIL import Image, ImageGrab, ImageColor
 import cfg
+
 
 class Clr:
     '''Convenience for specifying color'''
@@ -9,13 +12,64 @@ class Clr:
 
 CLR = Clr()
 
+
 def get_screenshot(bounding_box=None, idx=0, save=False):
     im = ImageGrab.grab(bounding_box.as_tuple())
     if save:
         im.save(f'shot{idx:04d}.jpg')
     return im
 
+
+class Rect:
+    '''Wrapper around a wintypes.RECT object'''
+    def __init__(self, r):
+        self.r: wintypes.RECT = r
+
+    def __str__(self):
+        x1, y1, x2, y2 = self.as_tuple()
+        width = x2 - x1
+        height = y2 - y1
+        return f"{x1},{x2}-{y1},{y2} {width}x{height}"
+
+    def as_tuple(self):
+        '''This is the format supported by Pillow'''
+        return self.r.left, self.r.top, self.r.right, self.r.bottom
+
+
+def get_win_location(desc) -> Rect :
+    user32 = ctypes.windll.user32
+    handle = user32.FindWindowW(None, desc)
+    rect = wintypes.RECT()
+    ff = ctypes.windll.user32.GetWindowRect(handle, ctypes.pointer(rect))
+    print(ff)
+    print(rect)
+    return Rect(rect)
+
+
+def get_game_image(args) -> Image:
+    if args.imgpath:
+        with Image.open(args.imgpath) as img:
+            print(f'Loading source image from {args.imgpath} instead of taking a screenshot')
+            img.load()  # allocate storage for the image
+    else:
+        r = get_win_location(cfg.WINDOW_DESC)
+        r.r.left += 8
+        r.r.right -= 8
+        r.r.top += 31
+        r.r.bottom -= 8
+
+        img = get_screenshot(r)
+        if args.save_screenshot:
+            now = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
+            fname = f'screenshot_{now}.jpg'
+            print(f'Saving screenshot to {fname}')
+            img.save(fname)
+    return img
+
+
+
 def clr(name):
+    '''Convenience function to get a color by name'''
     return ImageColor.getrgb(name)
 
 def alpha(color, a):
--- src/runner.py
+++ src/runner.py
@@ -2,8 +2,6 @@ import time
 import datetime
 import collections.abc
 import argparse
-import ctypes
-from ctypes import wintypes
 from PIL import Image, ImageFilter
 import keyboard
 import cfg
@@ -19,60 +17,14 @@ def wait_for_keypress(keys: list[str]) -> str:
             if keyboard.is_pressed(key):
                 return key
 
-
-class Rect:
-    '''Wrapper around a wintypes.RECT object'''
-    def __init__(self, r):
-        self.r: wintypes.RECT = r
-
-    def __str__(self):
-        x1, y1, x2, y2 = self.as_tuple()
-        width = x2 - x1
-        height = y2 - y1
-        return f"{x1},{x2}-{y1},{y2} {width}x{height}"
-
-    def as_tuple(self):
-        '''This is the format supported by Pillow'''
-        return self.r.left, self.r.top, self.r.right, self.r.bottom
-
-
-def get_win_location(desc) -> Rect :
-    user32 = ctypes.windll.user32
-    handle = user32.FindWindowW(None, desc)
-    rect = wintypes.RECT()
-    ff = ctypes.windll.user32.GetWindowRect(handle, ctypes.pointer(rect))
-    print(ff)
-    print(rect)
-    return Rect(rect)
-
-
-def get_game_image(args) -> Image:
-    if args.imgpath:
-        with Image.open(args.imgpath) as img:
-            print(f'Loading source image from {args.imgpath} instead of taking a screenshot')
-            img.load()  # allocate storage for the image
-    else:
-        r = get_win_location(cfg.WINDOW_DESC)
-        r.r.left += 8
-        r.r.right -= 8
-        r.r.top += 31
-        r.r.bottom -= 8
-
-        img = img_proc.get_screenshot(r)
-        if args.save_screenshot:
-            now = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
-            fname = f'screenshot_{now}.jpg'
-            print(f'Saving screenshot to {fname}')
-            img.save(fname)
-    return img
-
-
 def load_grid_cache():
     '''Calculate grid paths or load from disk'''
     return {}
 
 
 def preprocess_image(img: Image) -> Image:
+    '''Reduce the image down to a known list of colors to make parsing easier'''
+    print('Doing pre-processing on the image')
     palimage = Image.new('P', (16, 16))
     palimage.putpalette(cfg.PALETTE_COLORS)
     new_img = img.quantize(colors=len(cfg.PALETTE_COLORS) / 3, palette=palimage, dither=Image.Dither.NONE)
@@ -99,14 +51,14 @@ def main(args):
 
     if args.imgpath:
         # use provided image file for one iteration
-        img = get_game_image(args)
+        img = img_proc.get_game_image(args)
         process_image(img)
     else:
         # realtime - grab screenshot from game
         while True:
             key = wait_for_keypress(['x'])
             print(f'Keypress: {key}')
-            img = get_game_image(args)
+            img = img_proc.get_game_image(args)
             print(img)
             process_image(img)
 
